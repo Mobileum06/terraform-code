d03b3b084e0102b48d9861e7120b26111bd6fbef
Mobileum06 <manojchauhan1300@gmail.com>
2026-01-21 18:20:11 +0530

    Add files via upload

---
+++ Simple VM code/variable.tf
@@ -0,0 +1,15 @@
+variable "resource_group_name" {
+  type = string
+  default = "ManojResourceGroup"
+}
+
+variable "location" {
+  type = string   
+  default = "West Europe"
+}
+
+variable "vm_name" {
+  type = string
+default = "vm-1"
+    
+}
